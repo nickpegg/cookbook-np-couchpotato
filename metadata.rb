a43d130177eafae2cf82cfd2ee4804db0d775099
Nick Pegg <nick@nickpegg.com>
2018-03-14 18:41:13 -0700

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nick@nickpegg.com'
 license 'all_rights'
 description 'Installs/Configures np-couchpotato'
 long_description 'Installs/Configures np-couchpotato'
-version '0.1.2'
+version '0.1.3'
 
 supports 'debian', '~> 8.0'
 supports 'ubuntu', '= 14.04'
